d805c17bd1d81917f5957f8f9e3297ffba8e73d8
Alexkamilkov5 <anvarmuso73i@gmail.com>
2025-11-12 15:12:01 +0300

    fastapi +uvicorn

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+from fastapi import FastAPI
 import proekt320ShakirjanovXasan as p1
 import funcktiaInoyatov as p2
 from funcartur import artur
+
+
+
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
 
 print(p1.func1(2,3))
 print(p2.funk2Inoyatov(4,4))
