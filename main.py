2d821039791513a3e32dd77adba72c529f4d8696
ShakirjanovX <xasan7709@mail.ru>
2025-11-17 17:52:52 +0500

    add uvicorn fastapi

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 import proekt320ShakirjanovXasan as p1
 import funcktiaInoyatov as p2
 from funcartur import artur
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -13,3 +14,22 @@ print(p1.func1(2,3))
 print(p2.funk2Inoyatov(4,4))
 print(artur(6,3))
 print ("fastapi+uvicorn")
+
+
+app = FastAPI( title="MMM",
+version="1.0.0",
+description="Платформа для покупки и продажи",
+docs_url="/docs",
+redoc_url="/redoc",)
+
+class TwoNumbers(BaseModel):
+  x: float
+  y: float
+
+@app.get("/c2")
+def get_c2(x: float, y: float):
+ return {"result": p1.func1(x,y)}
+
+@app.post("/c2")
+def post_c2(data: TwoNumbers):
+ return {"result": p1.func1(data.x, data.y)}
